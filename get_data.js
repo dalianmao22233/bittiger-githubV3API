5b8bdbc174896df936c069fb13b08018e1bd2a82
dalianmao <dalianmao22233@users.noreply.github.com>
2016-07-25 21:08:46 -0700

    Update get_data.js

--- get_data.js
+++ get_data.js
@@ -12,7 +12,8 @@ var result_list = [];
 var options = {
     provider: 'google',
     httpAdapter: 'https', // Default
-    apiKey: 'AIzaSyAcpKWnvsLnzbOZHbhi_r4iGTgnOyuL77I', // for Mapquest, OpenCage, Google Premier
+    // apiKey: 'AIzaSyAcpKWnvsLnzbOZHbhi_r4iGTgnOyuL77I', // for Mapquest, OpenCage, Google Premier
+    apiKey: 'AIzaSyD6hh_2ix_whMciOODk-3vUNutCRzu31p4',
     formatter: null         // 'gpx', 'string', ...
 };
 
@@ -32,48 +33,42 @@ ref.on('value', function (snapshot) {
 
 // Or using Promise
     var someArray = [];
-    for(var k = 0; k < list.length; k++) {
+    for(var k = 0; k < uniquelist.length; k++) {
         someArray.push(k);
     }
     Promise.all(
         someArray.map(function(index) {
-            geocoder.geocode(list[index])
-                .then(function (res, status) {
-                    result_list.push(Number(res[0].latitude) + "," + Number(res[0].longitude) + "," + Number(0.2));
-                    // console.log(result_list);
-                    Promise.all(fs.writeFile("globe", result_list , function(err) {
-                        if(err) {
-                            return console.log(err);
-                        }
+            return new Promise(function(fulfill, rejection) {
+                geocoder.geocode(uniquelist[index])
+                    .then(function (res, status) {
+                        // console.log(res[0].formattedAddress);
+                        if(res[0] != null)
+                            result_list.push([Number(res[0].latitude) + "," + Number(res[0].longitude)]);
 
-                        console.log("The file was saved!");
-                    }))
-                        .catch(function (err) {
-                            console.log(err);
-                        })
+                        else
+                            result_list.push(0,0,0.5);
 
-                })
-                .catch(function (err) {
-                    result_list.push("0,0,0.5");
-                });
-            }))
+                        fulfill(result_list);
 
-            .catch (function (err) {
-                console.log(err);
+
+                    })
+                    .catch(function (err) {
+                        // result_list.push([77.2355, 28.552]);
+                        rejection(err);
+                    });
             })
+        }))
 
-            .then (function(){
-                // console.log("result: " + result_list);
+        .catch (function (err) {
+            console.log(err);
+        })
+
+        .then (function(){
+            console.log("result: " + result_list);
 
 
-                // fs.writeFile("test.txt", result_list , function(err) {
-                //     if(err) {
-                //         return console.log(err);
-                //     }
-                //
-                //     console.log("The file was saved!");
-                // });
-            })
+
+        })
 
 
 });
